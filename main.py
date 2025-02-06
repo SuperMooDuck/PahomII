a9abe8b34ad31933a3bc8f16b399d43b14989037
SuperMooDuck <smashercool@mail.ru>
2025-02-06 16:52:36 +1000

    .

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import logging
 from alarm_module import alarm
 from telebot import logger
 from storage_module import storage
-import bot_module
+
 
 logger.level = logging.DEBUG
 
+storage.bot_key = "7659409584:AAH7Mj-Sxj1RMIqu9MaHfv8iWRb3ofSl9Kw"
 storage.text_reactions["^да.?$"] = "Пизда"
 storage.text_reactions[r"(.+)-.*кто\?"] = r"\1 - гей"
+storage.save_data()
+
+import bot_module
 
 async def main():
     asyncio.create_task(alarm.WorkCycle())
